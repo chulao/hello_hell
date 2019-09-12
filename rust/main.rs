a523272fa758fd05c5a42d03865809b8bd60aa9f
Juliano Lao <juliano.lao@sec4you.com.br>
2019-09-12 20:05:23 -0300

    RUST: Adding function and closures

--- rust/main.rs
+++ rust/main.rs
@@ -2,10 +2,27 @@
  * Rust hello world
  */
 
+// Sum two numbers
+fn sum(x: i32, y: i32) -> i32 {
+  x + y
+}
+
+
 // Main function
 fn main() {
-  // Define the name
-  let name = "World !";
+  // Inline Function
+  fn pow (i: i32) -> i32 { i*2 }
 
-  println!("Hello {}", name);
+  // Closure
+  let add = |i: i32| -> i32 { i+1 };
+  let two = || 2;
+
+  // Define the name
+  let name = "world";
+  let value = pow(add(two()));
+  let total = sum(4, value);
+
+  // Print a hello
+  println!("Give hello {} for {} times", name, total);
+
 }
