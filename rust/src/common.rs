9ea94ea2e2ccd1426564357db0a12eaeffef005e
Juliano Lao <juliano.lao@sec4you.com.br>
2019-09-12 21:57:08 -0300

    RUST: Adding document into function with test validation embedded

--- rust/src/common.rs
+++ rust/src/common.rs
@@ -1,5 +1,12 @@
+/// A Math module
 pub mod math {
-  // Sum two numbers
+
+  /// Sum two numbers
+  ///
+  /// ```
+  /// let result = hello::common::math::sum(2, 3);
+  /// assert_eq!(result, 5);
+  /// ```
   pub fn sum(x: i32, y: i32) -> i32 {
     x + y
   }  
